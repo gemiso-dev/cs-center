4fbb97a3e31afe76b63ee6f5e7adfed49d324776
jongpilwonwon <125850672+jongpilwonwon@users.noreply.github.com>
2023-05-02 15:30:05 +0900

    Source UI

--- sequence_maker/ViewModels/MainWindowViewModel.cs
+++ sequence_maker/ViewModels/MainWindowViewModel.cs
@@ -4,12 +4,7 @@ namespace sequence_maker.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+        
 
         public MainWindowViewModel()
         {
